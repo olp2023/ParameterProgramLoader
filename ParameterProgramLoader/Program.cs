7af92f4227ead364a82d6defb828fb5346a63821
Corey Persson <223044990@ge.com>
2024-06-17 09:48:45 -0400

    add parameter setter

--- ParameterProgramLoader/Program.cs
+++ ParameterProgramLoader/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ParameterProgramLoader
 {
@@ -10,6 +11,36 @@ namespace ParameterProgramLoader
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Enter part number:");
+			var partName = Console.ReadLine();
+
+			var partFilePath = $@"C:\Temp\parts\{partName}.txt";
+
+			var partParameters = File.ReadAllLines(partFilePath)
+				.Select(p => p.Split(','))
+				.ToDictionary(p => p[0], p => p[1]);
+
+			var pcdmisType = Type.GetTypeFromProgID("PCDLRN.Application");
+			var pcdmis = Activator.CreateInstance(pcdmisType) as PCDLRN.Application;
+			var program = pcdmis.ActivePartProgram;
+			var commands = program.Commands;
+
+			for (int i = 1; i <= commands.Count; i++)
+			{
+				var command = commands.Item(i);
+				if (command.IsFlowControl)
+				{
+					var assignmentCommand = command.FlowControlCommand;
+					var assignmentName = assignmentCommand.GetLeftSideOfExpression();
+					var parameterExists = partParameters.TryGetValue(assignmentName, out string assignmentValue);
+					if (parameterExists)
+					{
+						assignmentCommand.SetRightSideOfAssignment(assignmentValue);
+					}
+
+				}
+			}
+
 		}
 	}
 }
